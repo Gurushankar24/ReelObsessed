4723853e572c07d01c7fdc1f658a56a903939fca
Gurushankar <gurushankarv24@gmail.com>
2026-03-01 14:31:40 +0530

    feat: add MovieService for movie search functionality
    - Implemented MovieService to interact with OMDB API for searching movies and fetching movie details by ID.
    - Added unit tests for MovieService to ensure proper functionality.
    - Updated package.json to include Angular CDK and Material dependencies.
    - Removed placeholder content from app.html and added initial structure for the application.
    - Updated index.html to include Google Fonts and Material Icons.
    - Cleared styles.scss for global styles.

---
+++ src/app/services/movie-service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable ,inject } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MovieService {
+ //  getinitalUrl :string =""
+  private apiKey = 'ca3dab5d'
+  private baseUrl = 'http://www.omdbapi.com'
+
+  http = inject(HttpClient);
+
+
+   searchMovies( query : string){
+    const url = this.baseUrl + `?s=${query}&apikey=${this.apiKey}`
+    return this.http.get<any>(url)
+   }
+
+   getMovieById( id : string){
+    const url = this.baseUrl + `?i=${id}&apikey=${this.apiKey}`
+    return this.http.get<any>(url)
+   }
+}
+   
