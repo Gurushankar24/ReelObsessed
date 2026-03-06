96a9f486bb9f46ce770101190033db0af07eeece
Gurushankar <gurushankarv24@gmail.com>
2026-03-06 21:00:13 +0530

    feat: update routing and styles for improved layout and structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { SearchPage } from './components/search-page/search-page';
 import { MovieDetails } from './components/movie-details/movie-details';
 
+
 export const routes: Routes = [
   
     {path:'search' , component : SearchPage},
-    {path: 'movie-details', component : MovieDetails}
+    {path: 'movie-details', component : MovieDetails},
+   
 ];
